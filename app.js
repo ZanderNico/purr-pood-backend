e0648bf6470f04a915289893c8b2824f4b7e1c25
Nico <106797953+ZanderNico@users.noreply.github.com>
2024-02-25 15:44:32 +0800

    create-routes-index

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const cors = require('cors')
 require('dotenv').config();
-const userRoute = require('./routes/user.routes')
-const petFoodRoute = require('./routes/petfood.routes')
-const cartsRoute = require("./routes/carts.routes")
 const { corsOptions } = require('./config/cors')
+const setRoutes = require("./routes/index")
 
 const app = express();
 
 app.use(cors(corsOptions))
 
 app.use(express.json());
-app.use('/users', userRoute)
-app.use('/petfood', petFoodRoute)
-app.use("/carts", cartsRoute)
+
+setRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
